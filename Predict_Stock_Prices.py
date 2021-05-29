57c54933c5f11fa6fc928ddd670cb32f2943519c
Jason Van <vanquocjason@gmail.com>
2021-05-28 23:02:27 -0400

    Stock Price Prediction Using Linear Regression

--- Predict_Stock_Prices.py
+++ Predict_Stock_Prices.py
@@ -19,7 +19,8 @@ class Ticker:
             else:
                 return True
 
-#pulls the necessary data
+
+# pulls the necessary data
 class GetData:
     def __init__(self, ticker):
         self.ticker = ticker
@@ -28,10 +29,11 @@ class GetData:
 
     def get_data(self):
         chosen_ticker = yf.Ticker(self.ticker)
-        raw_ticker_data = chosen_ticker.history(period="1d", start=self.begin_month, end=self.today_date)
+        raw_ticker_data = chosen_ticker.history(period="1d", start=self.begin_month, end=self.today_date, output='pandas')
         return raw_ticker_data
 
-#process the raw data
+
+# process the raw data
 class ProcessData():
     def __init__(self, t_data):
         self.ticker_data = t_data
@@ -40,10 +42,8 @@ class ProcessData():
         return days
 
     def prediction_column(self):
-        ticker_data['Prediction'] = ticker_data['Close'].shift(-1)# adds a column Predictions next to Close column
-        print(ticker_data['Prediction'])
-        ticker_data.dropna(inplace=True) #removes any missing values
-        print(ticker_data)
+        ticker_data['Prediction'] = ticker_data['Close'].shift(-1)  # adds a column Predictions next to Close column
+        ticker_data.dropna(inplace=True)  # removes any missing values
         return ticker_data
 
     def data_to_array(self):
@@ -51,13 +51,14 @@ class ProcessData():
         Y = np.array(ticker_data['Prediction'])
         return X, Y
 
-ticker_code = input().upper()
+
+ticker_code = input("Enter ticker.").upper()
 tk = Ticker(ticker_code)
 gd = GetData(ticker_code)
 ticker_data = gd.get_data()
 pd = ProcessData(ticker_data)
 try:
-    forecast_time = pd.get_days(int(input()))
+    forecast_time = pd.get_days(int(input('Enter number of days to forecast:')))
 except ValueError:
     print("Enter number of days.")
 ticker_data = pd.prediction_column()
@@ -69,5 +70,4 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5)
 clf = LinearRegression()
 clf.fit(x_train, y_train)
 prediction = (clf.predict(x_prediction))
-
 print(prediction)
